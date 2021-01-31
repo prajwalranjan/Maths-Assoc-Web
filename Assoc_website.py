2e721af5d3c49a2b4de28984e7677bbce280c78e
Ujjwaljain2608 <78258856+Ujjwaljain2608@users.noreply.github.com>
2021-01-31 17:55:31 +0530

    Add files via upload

---
+++ Assoc_website.py
@@ -0,0 +1,32 @@
+from flask import Flask , render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('Assoc_web_home.html')
+
+@app.route('/about')
+def about():
+    return render_template('Assoc_web_about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('Assoc_web_contact.html')
+
+@app.route('/join')
+def join(): 
+    return render_template('Assoc_web_join.html')
+
+@app.route('/join/join-status',methods = ['POST', 'GET'])
+def joinstatus(): 
+    if request.method == 'POST':
+        result = request.form
+        return render_template("join_data.html",result = result)
+ 
+if __name__ =='__main__':
+    app.run(debug=True)
+
+
+
+
+
