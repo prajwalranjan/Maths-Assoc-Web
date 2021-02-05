ed1d01bc660d7c88648885c9ba3ed381121b3880
Ujjwaljain2608 <78258856+Ujjwaljain2608@users.noreply.github.com>
2021-02-05 22:46:02 +0530

    Deployment updates

---
+++ static/js/join-us.js
@@ -0,0 +1,9 @@
+// const submitButton = document.querySelector(".button");
+
+// function congratulations(e){
+// 	alert("Congratulations on joining the mathematics association ");
+// }
+
+
+// submitButton.addEventListener("click", congratulations);
+alert("Hello");
