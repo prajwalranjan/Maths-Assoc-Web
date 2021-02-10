71c10db3f44c07d519c54141e5d6234af90345e5
BhrtAgrwl01 <f20190725@pilani.bits-pilani.ac.in>
2021-02-10 23:53:00 +0530

    Events-Upcoming&Past-Layout

---
+++ scripts/past-events.js
@@ -0,0 +1,36 @@
+const slide = document.querySelector(".events-slide");
+const images = document.querySelectorAll(".event");
+const left = document.querySelector(".left-arrow");
+const right = document.querySelector(".right-arrow");
+
+let counter = 1;
+const size = images[0].clientWidth;
+
+slide.style.transform = "translateX(" + (-size*counter) + "px";
+
+left.addEventListener("click", ()=>{
+    if(counter<=0) return;
+    slide.style.transition = "transform 0.4s ease-out";
+    counter--;
+    slide.style.transform = "translateX(" + (-size*counter) + "px";
+})
+
+right.addEventListener("click", ()=>{
+    if(counter>=images.length-1) return;
+    slide.style.transition = "transform 0.5s ease-out";
+    counter++;
+    slide.style.transform = "translateX(" + (-size*counter) + "px";
+})
+
+slide.addEventListener("transitionend", ()=>{
+    if(images[counter].id === "last-clone"){
+        slide.style.transition = "none";
+        counter=images.length-2;
+        slide.style.transform = "translateX(" + (-size*counter) + "px";
+    }
+    if(images[counter].id === "first-clone"){
+        slide.style.transition = "none";
+        counter=images.length-counter;
+        slide.style.transform = "translateX(" + (-size*counter) + "px";
+    }
+});
