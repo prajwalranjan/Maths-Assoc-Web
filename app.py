0640ae43a8dfd2f8228d4f76ed653d976e50cc00
Ujjwaljain2608 <78258856+Ujjwaljain2608@users.noreply.github.com>
2021-03-31 19:33:54 +0530

    Sendgrid integrated-mailing

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, redirect, flash, url_for
 import smtplib
 import ssl
 from flask_mail import Mail, Message
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 
 app = Flask(__name__)
@@ -74,39 +77,80 @@ def join():
 def contactstatus():
     if request.method == 'POST':
         formdetails = request.form
-        port = 465  # For SSL
-        smtp_server = "smtp.gmail.com"
-        sender_email = "mathsfirstperson@gmail.com"  # Enter your address
-        receiver_email = "maths.assoc@pilani.bits-pilani.ac.in"  # Enter receiver address
-        password = "maths1234"
-        message = """\
-        Subject: Contact details Mathematics Association, BITS Pilani
 
-        Name : {name}
-        Mail : {mail}
-        Contact No : {no}
-        Message : {message}""".format(name=formdetails["name"], mail=formdetails["mail"], no=formdetails["mobile"], message=formdetails["message"])
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-            server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, message)
+        message = Mail(
+            from_email='mathsfirstperson@gmail.com',
+            to_emails='maths.assoc@pilani.bits-pilani.ac.in',
+            subject='Contact Details, MABP Website',
+            plain_text_content="""\
+                Name : {name}
+                Mail : {mail}
+                Contact No : {no}
+                Message : {message}""".format(name=formdetails["name"], mail=formdetails["mail"], no=formdetails["mobile"], message=formdetails["message"]))
+        try:
+            sg = SendGridAPIClient(
+                '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')
+            response = sg.send(message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+        except Exception as e:
+            print(e)
+
+        # port = 465  # For SSL
+        # smtp_server = "smtp.gmail.com"
+        # sender_email = "mathsfirstperson@gmail.com"  # Enter your address
+        # receiver_email = "maths.assoc@pilani.bits-pilani.ac.in"  # Enter receiver address
+        # password = "maths1234"
+        # message = """\
+        # Subject: Contact details Mathematics Association, BITS Pilani
+
+        # Name : {name}
+        # Mail : {mail}
+        # Contact No : {no}
+        # Message : {message}""".format(name=formdetails["name"], mail=formdetails["mail"], no=formdetails["mobile"], message=formdetails["message"])
+        # context = ssl.create_default_context()
+        # with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        #     server.login(sender_email, password)
+        #     server.sendmail(sender_email, receiver_email, message)
     return redirect(url_for("home"))
 
 
-@app.route('/join/join-status', methods=['POST', 'GET'])
+@ app.route('/join/join-status', methods=['POST', 'GET'])
 def joinstatus():
     if request.method == 'POST':
         formdetails = request.form
 
-        msg = Message('Joining details Mathematics Association, BITS Pilani',
-                      sender='mathsfirstperson@gmail.com', recipients=['mathssecondperson@gmail.com'])
-        msg.body = """\
-        Name : {name}
-        BITS ID : {id}
-        Contact No : {no}
-        BITS Email : {mail}""".format(name=formdetails["name"], id=formdetails["BITS ID"], no=formdetails["mobile"], mail=formdetails["mail"])
-        mail.send(msg)
-        return redirect(url_for("home"))
+        message = Mail(
+            from_email='mathsfirstperson@gmail.com',
+            to_emails='maths.assoc@pilani.bits-pilani.ac.in',
+            subject='Joining Details, MABP Website',
+            plain_text_content="""\
+                Name : {name}
+                BITS ID : {id}
+                Contact No : {no}
+                BITS Email : {mail}""".format(name=formdetails["name"], id=formdetails["BITS ID"], no=formdetails["mobile"], mail=formdetails["mail"]))
+        try:
+            sg = SendGridAPIClient(
+                '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')
+            response = sg.send(message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+        except Exception as e:
+            print(e)
+
+    return redirect(url_for("home"))
+
+    # msg = Message('Joining details Mathematics Association, BITS Pilani',
+    #               sender='mathsfirstperson@gmail.com', recipients=['mathssecondperson@gmail.com'])
+    # msg.body = """\
+    # Name : {name}
+    # BITS ID : {id}
+    # Contact No : {no}
+    # BITS Email : {mail}""".format(name=formdetails["name"], id=formdetails["BITS ID"], no=formdetails["mobile"], mail=formdetails["mail"])
+    # mail.send(msg)
+    # return redirect(url_for("home"))
 
     #     port = 465  # For SSL
     #     smtp_server = "smtp.gmail.com"
